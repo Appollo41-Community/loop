857286c2efc0d775209c1f29de519d0aad45529e
jovanSimonovic <jsimonovic7021rn@raf.rs>
2024-09-14 19:18:01 +0200

    Added Koin for Room implemetation.

---
+++ composeApp/src/iosMain/kotlin/com.appollo41/loop/di/modules/sharedModule.ios.kt
@@ -0,0 +1,10 @@
+package com.appollo41.loop.di.modules
+
+import com.appollo41.loop.db.AppDatabase
+import com.appollo41.loop.db.getDatabaseBuilder
+import org.koin.core.module.Module
+import org.koin.dsl.module
+
+actual fun platformModule() = module {
+    single<AppDatabase> { getDatabaseBuilder() }
+}
